8668927fdf97cf6770ab7fceef1389524cb33f38
Juan Pablo Cortez Mosquera <63519906+JuanpaCortez93@users.noreply.github.com>
2026-03-24 00:36:44 -0500

    functions-destructuring

---
+++ src/topics/06-functions-destructuring.ts
@@ -0,0 +1,36 @@
+interface Product {
+    description:string;
+    price:number;
+}
+
+const phone : Product = {
+    description: 'Nokia A1',
+    price: 150
+}
+
+const tablet : Product = {
+    description: 'iPad',
+    price: 450
+}
+
+interface TaxCalculationOptions {
+    tax:number;
+    products:Product[];
+}
+
+function taxCalculation({tax, products}:TaxCalculationOptions) : number[] {
+    let total = 0;
+    products.forEach( product => total += product.price);
+    return [total, total*tax];
+}
+
+
+const shoppingCart = [phone, tablet];
+const tax = 0.15;
+
+const result = taxCalculation({tax, products: shoppingCart});
+
+const [total, finalTax] = result;
+console.log({total, finalTax});
+
+export {};
